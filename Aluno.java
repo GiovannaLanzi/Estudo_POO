22a7fada8f14b8a96a175d03a52ca7a130b9a6b6
Giovanna Lanzi Lopes Torres <160290107+GiovannaLanzi@users.noreply.github.com>
2024-10-20 20:47:03 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,25 @@
+package uscs;
+public class Aluno extends Pessoa{
+    private int matr;
+    private String curso;
+    
+    public Aluno(){
+    }
+    public void cancelarMatr(){
+        System.out.println("Matrícula será cancelada");
+    }
+    
+    public int getMatr(){
+        return matr;
+    }
+    public void setMatr(int matr){
+        this.matr = matr;
+    }
+    public String getCurso(){
+        return curso;
+    }
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+    
+}
